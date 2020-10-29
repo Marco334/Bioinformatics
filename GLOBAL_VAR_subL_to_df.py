4c32b5f76e93c411d944c00890fe791519202518
Marco Genca <54363103+Marco334@users.noreply.github.com>
2020-10-29 16:53:45 +0100

    Update GLOBAL_VAR_subL_to_df.py
    How to extract global Variables sub-list and create a Dataframe with related values.

--- GLOBAL_VAR_subL_to_df.py
+++ GLOBAL_VAR_subL_to_df.py
@@ -4,13 +4,13 @@ import pandas
 def slicedict(d, s):
     ''' filter variables name'''
     return {k:v for k,v in tt.items() if str(k).startswith(s) }
-    
+
 def FILT_GLOB_VARDICT_to_DF(tt, s):
    ''' filter global variables dictionary and create dataFrame'''
     data_GV=slicedict(tt, s)
     GV_df = pd.DataFrame.from_dict(data_GV, orient='index' )
     return(GV_df) 
-    
+
 tt = vars()
 s = 'SUBSTRING_'
 print(FILT_GLOB_VARDICT_to_DF(tt, s))
